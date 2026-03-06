e79790448a229cdb0d0910d49812a50bed1dc9a7
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2026-03-05 21:31:25 -0300

    Criando conexão

---
+++ GoogleDrive.Console/ArquivoLog.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleDrive.Console
+{
+    public class ArquivoLog
+    {
+        private readonly string _caminhoLog;
+
+        public ArquivoLog()
+        {
+
+            string basePath = @"C:\"; //AppDomain.CurrentDomain.BaseDirectory; // @"C:\";
+
+            _caminhoLog = Path.Combine(basePath, "Logs");
+            if (!Directory.Exists(_caminhoLog))
+            {
+                Directory.CreateDirectory(_caminhoLog);
+            }
+
+            Info(_caminhoLog);
+
+        }
+
+        public async Task<string> IncluirLinha(Exception ex, string requestPath, string mensagemBasica)
+        {
+
+            // Nome do arquivo com base na data
+            string nomeArquivo = Path.Combine(_caminhoLog, $"error_log_{DateTime.Now:dd-MM-yyyy}.txt");
+
+            string separador = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} -----------------------------------------------------------------------------{Environment.NewLine}";
+            string mensagemRetorno = $"TratamentoErroMiddleware | Path: {requestPath} | {mensagemBasica}: {ex.Message}{Environment.NewLine}";
+
+            var mensagemPersonalizada = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {mensagemRetorno}{Environment.NewLine}";
+
+            File.AppendAllText(nomeArquivo, separador);
+            File.AppendAllText(nomeArquivo, mensagemPersonalizada);
+
+            System.Console.WriteLine($"Erro: {mensagemPersonalizada}");
+
+            Error(mensagemPersonalizada);
+
+
+            return mensagemRetorno;
+        }
+
+        public static void Padrao(string menssagem, ConsoleColor foreground, ConsoleColor background)
+        {
+            System.Console.BackgroundColor = background;
+            System.Console.ForegroundColor = foreground;
+            System.Console.WriteLine(menssagem);
+            System.Console.ResetColor(); // Restaura as cores padrão após a mensagem.
+        }
+
+
+        public void Error(string menssagem)
+        {
+            Padrao($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {menssagem}", ConsoleColor.White, ConsoleColor.Red);
+        }
+
+
+        public void Sucesso(string menssagem)
+        {
+            Padrao($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {menssagem}", ConsoleColor.Black, ConsoleColor.Green);
+        }
+
+
+        public void Alerta(string menssagem)
+        {
+            Padrao($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {menssagem}", ConsoleColor.Black, ConsoleColor.Yellow);
+        }
+
+        public void Info(string menssagem)
+        {
+            Padrao($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {menssagem}", ConsoleColor.Yellow, ConsoleColor.Blue);
+        }
+
+        public void Padrao(string menssagem)
+        {
+            Padrao($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {menssagem}", ConsoleColor.White, ConsoleColor.Black);
+        }
+
+    }
+}
---
+++ GoogleDrive.Console/Program.cs
@@ -0,0 +1,103 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using GoogleDrive.Console;
+using System.Text.Json;
+
+//https://developers.google.com/workspace/drive/api/reference/rest/v3?hl=pt-br
+//https://developers.google.com/workspace/drive/api/guides/folder?hl=pt-br#.net
+//https://console.cloud.google.com/cloud-resource-manager?project=natural-motif-489312-q4
+
+ArquivoLog _arquivoLog = new ArquivoLog();
+GoogleDriveConfiguracaoDto _GoogleDriveConfiguracaoDto = new GoogleDriveConfiguracaoDto();
+string _pasta = string.Empty;
+string _ApplicationName = string.Empty;
+
+
+var caminhoConfig = @"C:\Amauri\GitHub\GOOGLEDRIVE_CONFIGURACAO.txt";
+if (File.Exists(caminhoConfig))
+{
+    var json = await File.ReadAllTextAsync(caminhoConfig);
+    _GoogleDriveConfiguracaoDto = JsonSerializer.Deserialize<GoogleDriveConfiguracaoDto>(json);
+    if (_GoogleDriveConfiguracaoDto == null)
+    {
+        _arquivoLog.Error("Configuração do Dropbox está vazia.");
+        return;
+    }
+}
+_ApplicationName = _GoogleDriveConfiguracaoDto.ApplicationName;
+_pasta = _GoogleDriveConfiguracaoDto.Pasta;
+_arquivoLog.Padrao($" - RedirectUri: {_ApplicationName}");
+_arquivoLog.Padrao($" - Pasta: {_pasta}");
+
+
+
+string[] Scopes = { DriveService.Scope.DriveFile };
+UserCredential credential;
+
+
+try
+{
+    // 1. Autenticação
+    using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+    {
+        string credPath = "token.json";
+        credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+            GoogleClientSecrets.FromStream(stream).Secrets,
+            Scopes,
+            "user",
+            CancellationToken.None,
+            new FileDataStore(credPath, true));
+    }
+
+    var service = new DriveService(new BaseClientService.Initializer()
+    {
+        HttpClientInitializer = credential,
+        ApplicationName = _ApplicationName,
+    });
+
+    // 2. Criar uma Pasta
+    var folderMetadata = new Google.Apis.Drive.v3.Data.File()
+    {
+        Name = _pasta,
+        MimeType = "application/vnd.google-apps.folder"
+    };
+    var request = service.Files.Create(folderMetadata);
+    request.Fields = "id";
+    var folder = await request.ExecuteAsync();
+    Console.WriteLine("Pasta criada. ID: " + folder.Id);
+
+    // 3. Enviar um Arquivo para essa pasta
+    var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+    {
+        Name = "TesteUpload.txt",
+        Parents = new List<string> { folder.Id } // Define a pasta pai
+    };
+
+    FilesResource.CreateMediaUpload requestUpload;
+    using (var stream = new FileStream("arquivo_local.txt", FileMode.Open))
+    {
+        requestUpload = service.Files.Create(fileMetadata, stream, "text/plain");
+        requestUpload.Fields = "id";
+        await requestUpload.UploadAsync();
+    }
+
+    var file = requestUpload.ResponseBody;
+    Console.WriteLine("Arquivo enviado com sucesso! ID: " + file.Id);
+}
+catch (Exception ex)
+{
+    _arquivoLog.Error($"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    _arquivoLog.Error($" Erro: {ex.Message} / {ex.InnerException?.Message ?? ""}");
+    _arquivoLog.Error($"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n");
+}
+
+
+
+public class GoogleDriveConfiguracaoDto
+{
+    public string ApplicationName { get; set; } = "";
+    public string Pasta { get; set; } = "";
+
+}
